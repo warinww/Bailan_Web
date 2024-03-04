e38c3cff77092ae977525e49e17c5ac0f9431119
warinww <warinrinrin14@gmail.com>
2024-03-04 13:44:37 +0700

    new commit

--- main.py
+++ main.py
@@ -71,13 +71,13 @@ controller.add_promotion_list(promotion2)
 
 reader1.update_book_collection_list(book1)
 
-# @app.get("/bookinfo", tags=['Book'])
-# async def get_book_info(id:int) -> dict:
-#     return {"Book's info": controller.show_book_info(id)}
+@app.get("/bookinfo", tags=['Book'])
+async def get_book_info(id:int) -> dict:
+    return {"Book's info": controller.show_book_info(id)}
 
-# @app.post("/transfer", tags=['money'])
-# async def transfer_coin_to_money(writer_id:int, data: coinInput):
-#     return {controller.transfer(writer_id, data.coin)}
+@app.post("/transfer", tags=['money'])
+async def transfer_coin_to_money(writer_id:int, data: coinInput):
+    return {controller.transfer(writer_id, data.coin)}
 
 @app.post("/rent", tags=['Cart'])
 async def rent(reader_id: int, data: BookIdList):
